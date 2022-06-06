f4ac1bd09c5f81a194222b656eef09ec87410d2b
Luiz Repiso <luizrepiso@yahoo.es>
2022-06-06 16:06:17 -0300

    UserSearch changes

--- src/components/ResultCard/index.tsx
+++ src/components/ResultCard/index.tsx
@@ -1,16 +1,54 @@
 import './styles.css';
 
 type Props = {
-  title: string;
-  description: string;
+  title?: string;
+  description?: string;
+  url?: string;
+  html_url?: string;
+  followers?: string;
+  location?: string;
+  name?: string;
 };
 
-const ResultCard = ({ title, description }: Props) => {
+const ResultCard = ({ 
+  title, description, url, html_url, 
+  followers,  
+  location, 
+  name, }: Props) => {
     
+
   return (
+
     <div className="result-container">
-      <h3 className="result-title">{title}</h3>
-      <p className="result-description">{description}</p>
+      <div className="img-container">
+        <img src={url} alt={title} />
+      </div>
+
+      <div className="information-container">
+        <div>
+        <h2 className="information-title">Informações</h2>
+        </div>
+        <div>
+        <h3 className="information-profile">
+          Perfil: <a href={html_url}>{html_url}</a>
+        </h3>
+        </div>
+        <div>
+        <h3 className="information-followers">
+          Seguidores: <span className="info-followers">{followers}</span>
+        </h3>
+        </div>
+        <div>
+        <h3 className="information-location">
+          Localidade: <span className="info-location">{location}</span>{' '}
+        </h3>
+        </div>
+        <div>
+        <h3 className="information-name">
+          Nome: <span className="info-name">{name}</span>{' '}
+        </h3>
+        </div>
+      </div>
     </div>
   );
 };
